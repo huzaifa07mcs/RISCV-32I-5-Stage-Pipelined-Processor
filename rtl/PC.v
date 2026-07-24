4c7441dc94d87ff946742257b3b215f3ec989477
Huzaifa <huzaifa.bee24mcs@student.nust.edu.pk>
2026-07-24 19:13:46 +0500

    Add program counter module with reset functionality

--- rtl/PC.v
+++ rtl/PC.v
@@ -1,1 +1,16 @@
+module programcounter #(
+    parameter XLEN = 32
+)(
+    input clk,
+    input reset,
+    input [XLEN-1:0] pc_next,
+    output reg [XLEN-1:0] pc_current
+);
 
+always @(posedge clk) begin
+    if (reset)
+        pc_current <= {XLEN{1'b0}};
+    else
+        pc_current <= pc_next;
+end
+endmodule
