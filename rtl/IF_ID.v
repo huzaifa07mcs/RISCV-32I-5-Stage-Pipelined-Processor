4a212f99a0d961df425feb82fb4b816f367a8ea8
Huzaifa <huzaifa.bee24mcs@student.nust.edu.pk>
2026-07-28 20:47:43 +0500

    Reorder conditions in IF_ID module
    Rearranged the order of conditions in the always block to handle flush before stall.

--- rtl/IF_ID.v
+++ rtl/IF_ID.v
@@ -4,23 +4,27 @@ module IF_ID(
     output reg [31:0] pc_current_out, pc_plus4_out, instruction_out
     );
 always@(posedge clk)
+    
 if(reset) begin
 pc_current_out <= 32'b0;
 pc_plus4_out <= 32'b0;
 instruction_out <= 32'b0;
 end
-else if (stall)
-begin
-pc_current_out <= pc_current_out;
-pc_plus4_out <= pc_plus4_out;
-instruction_out <= instruction_out;
-end
+    
 else if (flush)
 begin
 pc_current_out <= 32'b0;
 pc_plus4_out <= 32'b0;
 instruction_out <= 32'b0;
 end
+    
+else if (stall)
+begin
+pc_current_out <= pc_current_out;
+pc_plus4_out <= pc_plus4_out;
+instruction_out <= instruction_out;
+end
+    
 else
 begin
 pc_current_out <= pc_current_in;
