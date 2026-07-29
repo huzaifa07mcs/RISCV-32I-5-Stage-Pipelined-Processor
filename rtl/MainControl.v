9caef9c95e80170fd5c44ac1c051329de463fed2
Huzaifa <huzaifa.bee24mcs@student.nust.edu.pk>
2026-07-29 19:00:44 +0500

    Update MainControl.v

--- rtl/MainControl.v
+++ rtl/MainControl.v
@@ -1,6 +1,6 @@
 module MainControl(
     input [6:0] opcode,
-    output reg RegWrite, ALUSrc, MemWrite, MemtoReg, Branch, Jump,
+    output reg RegWrite, ALUSrc, MemWrite, MemToReg, Branch, Jump,
     output reg [1:0] ALUOp, ImmSrc
     );
 always@(*)
@@ -8,7 +8,7 @@ begin
     RegWrite = 1'b0;
     ALUSrc   = 1'b0;
     MemWrite = 1'b0;
-    MemtoReg = 1'b0;
+    MemToReg = 1'b0;
     Branch   = 1'b0;
     Jump     = 1'b0;
     ALUOp    = 2'bxx;
@@ -30,7 +30,7 @@ begin
         7'b0000011: begin // LW
             RegWrite = 1'b1;
             ALUSrc   = 1'b1;
-            MemtoReg = 1'b1;
+            MemToReg = 1'b1;
             ALUOp    = 2'b00;
             ImmSrc   = 2'b00;
         end
@@ -38,13 +38,13 @@ begin
         7'b0100011: begin // SW
             MemWrite = 1'b1;
             ALUSrc   = 1'b1;
-            MemtoReg = 1'bx;
+            MemToReg = 1'bx;
             ALUOp    = 2'b00;
             ImmSrc   = 2'b01;
         end
 
         7'b1100011: begin // BEQ/BNE
-            MemtoReg = 1'bx;
+            MemToReg = 1'bx;
             Branch   = 1'b1;
             ALUOp    = 2'b01;
             ImmSrc   = 2'b10;
@@ -53,14 +53,14 @@ begin
         7'b1101111: begin // JAL
             RegWrite = 1'b1;
             ALUSrc   = 1'bx;
-            MemtoReg = 1'bx;
+            MemToReg = 1'bx;
             ALUOp    = 2'bxx;
             Jump     = 1'b1;
             ImmSrc   = 2'b11;
         end
 
         default: begin // unsupported opcode
-            RegWrite = 0; ALUSrc = 0; MemWrite = 0; MemtoReg = 0;
+            RegWrite = 0; ALUSrc = 0; MemWrite = 0; MemToReg = 0;
             Branch = 0;   Jump = 0;   ALUOp = 2'b00; ImmSrc = 2'b00;
         end
     endcase
