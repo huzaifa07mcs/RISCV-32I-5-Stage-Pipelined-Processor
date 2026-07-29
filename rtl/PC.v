bced5f9c1f765f5b7833283546cef11b32833a01
Huzaifa <huzaifa.bee24mcs@student.nust.edu.pk>
2026-07-29 15:34:31 +0500

    Add stall input to PC module for holding state

--- rtl/PC.v
+++ rtl/PC.v
@@ -3,13 +3,15 @@ module programcounter #(
 )(
     input clk,
     input reset,
+    input stall,
     input [XLEN-1:0] pc_next,
     output reg [XLEN-1:0] pc_current
 );
-
 always @(posedge clk) begin
     if (reset)
         pc_current <= {XLEN{1'b0}};
+    else if (stall)
+        pc_current <= pc_current;   // hold 
     else
         pc_current <= pc_next;
 end
